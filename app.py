48761e4ffeac5e0b20b4cb06e8a3f6722f7fb142
Baseer Baheer <b.baheer@xu-university.de>
2024-04-17 14:36:23 +0200

    IDEA removed

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
-print ("Hello Data Analysts")
-a = 89
-b = 78
-c = 100
-
-srt = Math.sqrt(c)
-
-print ( str(a+b+c))
+if 2 > 5:
+    print("Five is greater than two!")
+else:
+    print("The IF Statement is false")
