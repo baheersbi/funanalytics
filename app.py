c5f6db5d9bf5a75cc29d4e77acf92e86a7d0ff38
baheersbi <baseer.baheer@berlinsbi.com>
2024-04-17 15:54:30 +0200

    Calculator Added

--- app.py
+++ app.py
@@ -1,4 +1,20 @@
-if 2 > 5:
-    print("Five is greater than two!")
-else:
-    print("The IF Statement is false")
+# print ("Hello World")
+
+print("Welcome to my State-of-the-art Calculator")
+op = ['Addition', 'Subtraction', 'Multiplication', "Division"]
+
+i = 1
+for x in op:
+    print(str(i) + ". " +x)
+    i +=1
+
+a1 = input("Please enter the first value:")
+a2 = input("Please enter the second value:")
+
+a = input("Please choose the operation:")
+if a=='1':
+    print("The sum of these two numbers: " + str(int(a1)+int(a2)))
+
+
+
+
